6eb52101d654e48d0b17a3d572a7037745c86933
Harald Puhr <harald.puhr@gmail.com>
2025-02-19 15:54:28 +0100

    Deactivate venv

--- paperpy.sh
+++ paperpy.sh
@@ -4,4 +4,5 @@ cd /home/pi/paperpy
 rclone move --verbose --log-file=logs/rclone.log dropbox:Privat/PaperPyIn files_new
 . paperpy/bin/activate
 python3 paper.py >> logs/paperpy.log 2>&1
+deactivate
 rclone move --verbose --log-file=logs/rclone.log files_old dropbox:Privat/PaperPyOut
